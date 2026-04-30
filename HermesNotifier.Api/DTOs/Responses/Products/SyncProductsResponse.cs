05f97ac3daf59d6e742b5fd962c5cec01fb0e97f
feng <tony0970941055@gmail.com>
2026-04-30 23:52:42 +0800

    feat: sync response

--- HermesNotifier.Api/DTOs/Responses/Products/SyncProductsResponse.cs
+++ HermesNotifier.Api/DTOs/Responses/Products/SyncProductsResponse.cs
@@ -3,10 +3,7 @@ namespace HermesNotifier.Api.DTOs.Responses.Products;
 public class SyncProductsResponse
 {
     public int AddedCount { get; set; }
-
     public int DeletedCount { get; set; }
-
     public int NotifiedCount { get; set; }
-
     public string Message { get; set; } = string.Empty;
 }
